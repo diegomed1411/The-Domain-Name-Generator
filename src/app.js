102a7a02fd04703568c1050b87952bcba97a2b8e
Diego Medina <cr.diegomedina@gmail.com>
2021-08-28 00:36:33 +0000

    nuevo ejercicio
    esta vez sin accidente
    
    gracias Antonio por tu ayuda

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+pronoun.forEach(pronoun => {
+  adj.forEach(adj => {
+    noun.forEach(noun => console.log(pronoun+adj+noun))
+  })
+})
+;
 };
